edcce0a98232c4b8e70c9386185164208f375798
Prabhakar Gupta <Prabhakarg465@gmail.com>
2026-09-08 23:07:34 +0530

    Revert "Add authentication UI"

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -1,21 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile_app/app.dart';
-import 'package:mobile_app/core/constants/app_constants.dart';
-import 'package:mobile_app/features/authentication/auth_controller.dart';
-import 'package:provider/provider.dart';
+import 'package:mobile_app/main.dart';
 
 void main() {
-  testWidgets('Authentication Screen renders cleanly',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ChangeNotifierProvider<AuthController>(
-        create: (_) => AuthController(),
-        child: const HoneyChainApp(),
-      ),
-    );
-
-    // Verify App Name & Login Button
-    expect(find.text(AppConstants.appName), findsOneWidget);
-    expect(find.text(AppConstants.loginButtonText), findsOneWidget);
+  testWidgets('App load test', (WidgetTester tester) async {
+    await tester.pumpWidget(const HoneyChainApp());
+    expect(find.text('HoneyChain Mobile Application'), findsOneWidget);
   });
 }
